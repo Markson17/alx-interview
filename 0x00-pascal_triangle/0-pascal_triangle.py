c9f4a1baf9c81d228556b511efba7537a5024346
Markson17 <marksonfrosh@gmail.com>
2023-07-06 03:25:04 +0100

    updated the files

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,23 @@
+#!/usr/bin/python3
+'''A module for working with Pascal's triangle.
+'''
+
 def pascal_triangle(n):
-    if n <= 0:
-        return []  # Return an empty list if n is less than or equal to 0
-
-    triangle = [[1]]  # Initialize the triangle with the first row [1]
-
-    for i in range(1, n):
-        row = [1]  # Start each row with 1
-        for j in range(1, i):
-            # Calculate the value for each position in the current row by summing the corresponding elements from the previous row
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)  # End each row with 1
-        triangle.append(row)  # Append the row to the triangle
-
-    return triangle  # Return the Pascal's triangle as a list of lists
-
-
-
-def print_triangle(n):
-    """
-    Print the Pascal's triangle
-    """
-    triangle = pascal_triangle(n)
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    triangle = []
+    
+    if not isinstance(n, int) or n <= 0:
+        return triangle
+    
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    
+    return triangle
