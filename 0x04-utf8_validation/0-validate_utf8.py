be9b424b7e1849d49242c24adbc4e38178139f22
Markson17 <marksonfrosh@gmail.com>
2023-07-31 02:43:30 +0100

    updated the validate utf8 task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,16 +6,13 @@ def validUTF8(data):
     represents a valid utf-8 encoding
     """
     number_bytes = 0
-
     mask_1 = 1 << 7
     mask_2 = 1 << 6
 
     for i in data:
-
         mask_byte = 1 << 7
 
         if number_bytes == 0:
-
             while mask_byte & i:
                 number_bytes += 1
                 mask_byte = mask_byte >> 1
@@ -32,7 +29,4 @@ def validUTF8(data):
 
         number_bytes -= 1
 
-    if number_bytes == 0:
-        return True
-
-    return False
+    return number_bytes == 0
